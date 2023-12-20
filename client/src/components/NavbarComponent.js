1ec3ad25c559b19d6510fd839ddd4fefa8ea8413
PlamenCode <plamenggg233@gmail.com>
2023-12-20 18:52:02 +0200

    creating cart context

--- client/src/components/NavbarComponent.js
+++ client/src/components/NavbarComponent.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import { Button, Navbar } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Modal, Navbar } from 'react-bootstrap';
 
 export default function NavbarComponent() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
   return (
+    <>
     <Navbar expand='sm' >
         <Navbar.Brand href='/'>Ecommerce Store</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className='justify-content-end' >
-            <Button>Cart 0 Items</Button>
+            <Button onClick={handleShow} >Cart 0 Items</Button>
         </Navbar.Collapse>
     </Navbar>
+    <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton >
+            <Modal.Title>Shopping Cart</Modal.Title>
+        </Modal.Header>
+            <Modal.Body>
+                <h1>This is the modal body</h1>
+            </Modal.Body>
+    </Modal>
+    </>
+
   )
 }
---
+++ client/src/context/CartContext.js
@@ -0,0 +1,91 @@
+import React, { createContext, useState } from 'react';
+import { getProductData, productsArray } from '../productsStore';
+
+//Context (cart, addToCart, removeFromCart, ...)
+//Provider -> gives React app access to all the things in the context
+
+export const CartContext = createContext({
+    items: [],
+    getProductQuantity: () => {},
+    addToCart: () => {},
+    removeFromCart: () => {},
+    deleteFromCart: () => {},
+    getTotalCost: () => {}
+});
+
+export function CartProvider({ children }){
+    const [cartProducts, setCartProducts] = useState([]);
+
+    function getProductQuantity(id){
+        const quantity =  cartProducts.find(product => product.id === id)?.quantity;
+        if(quantity === undefined){
+            return 0;
+        }
+        return quantity;
+    };
+
+    function addToCart(id){
+        const quantity = getProductQuantity(id);
+        if(quantity === 0){ //Product not in cart
+            setCartProducts(
+                [...cartProducts, { id, quantity: 1 }]
+            )
+        } else {//Product is in cart already
+            setCartProducts(
+                cartProducts.map(product => 
+                    product.id === id 
+                    ? {...product, quantity: product.quantity + 1}
+                    : product
+                )
+            )
+        }
+    };
+
+    function removeFromCart(id){
+        const quantity = getProductQuantity(id);
+        if(quantity === 1){
+            deleteFromCart(id);
+        } else {
+            setCartProducts(
+                cartProducts.map(product => 
+                    product.id === id 
+                    ? {...product, quantity: product.quantity - 1}
+                    : product
+                )
+            )
+        }
+    };
+
+    function deleteFromCart(id){
+        setCartProducts(
+            cartProducts => 
+                cartProducts.filter(currProduct => {
+                    return currProduct.id !== id 
+                })
+        )
+    };
+
+    function getTotalCost(){
+        let totalCost = 0;
+        cartProducts.map((cartItem) => {
+            const productData = getProductData(cartItem.id);
+            totalCost += (productData.price * cartItem.quantity)
+        });
+        return totalCost;
+    }
+
+    const contextValue = {
+        items: cartProducts,
+        getProductQuantity,
+        addToCart,
+        removeFromCart,
+        deleteFromCart,
+        getTotalCost
+    }
+
+    return (
+        <CartContext.Provider value={contextValue}>
+            {children}
+        </CartContext.Provider>
+    )
+}
